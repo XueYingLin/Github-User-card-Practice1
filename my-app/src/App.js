1868d6d009611f11f6695fe6c98fd04dcbe1e91a
Nikki Lin <nikkiylin@gmail.com>
2020-03-11 00:42:09 -0700

    added "styles.css"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import DisplayCard from "./components/DisplayCard";
+import axios from "axios";
+import "./components/styles.css";
 
 import './App.css';
+
 
 class App extends React.Component {
   state = {
@@ -18,12 +22,33 @@ class App extends React.Component {
     })
     .catch(err => console.log(err.message));
 
-    
+    axios
+    .get('https://api.github.com/users/XueYingLin/followers')
+    .then(res => {
+      this.setState({
+        followers: res.data
+      });
+    })
+    .catch(err => console.log(err.message));
   }
   render() {
   return (
     <div className="App">
-      
+      <DisplayCard avatar_url={this.state.profile.avatar_url}
+               login={this.state.profile.login}
+               html_url={this.state.profile.html_url}
+               email={this.state.profile.email} />
+
+      <p className="text">Followers as bellow</p>
+
+      {this.state.followers.map(follower => (
+        <DisplayCard avatar_url={follower.avatar_url}
+               login={follower.login}
+               html_url={follower.html_url}
+               email={follower.email} />
+      ))}
+
+
         
         
     </div>
